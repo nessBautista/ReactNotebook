64c8295f9a4ab28315d3ffba196ad0363954738e
Ness Bautista <ness@Nesss-MacBook-Pro.local>
2020-05-16 13:52:47 -0500

    Exploring JSX: conditional operators

--- reactFoundations/indecision-app/public/scripts/app.js
+++ reactFoundations/indecision-app/public/scripts/app.js
@@ -2,13 +2,84 @@
 
 console.log("App.js is running");
 
+var user = {
+  name: "Andrew",
+  age: 26,
+  location: "Texas"
+};
+
+var app = {
+  title: "This is the title",
+  subtitle: "This is a subtitle",
+  options: ["one", "two"]
+};
+var userName = "John Wick";
+var userAge = 43;
+var userLocation = "New York";
+
+function getOptions(app) {
+  return React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Options" : "No options"
+  );
+}
 //JSX-Javascript XML
 var template = React.createElement(
-  "p",
+  "div",
   null,
-  "This is JSX code with watch!"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "item two"
+    )
+  )
+);
+
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name.toUpperCase()
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: ",
+    user.location
+  )
 );
 
 var appRoot = document.getElementById("app");
-
 ReactDOM.render(template, appRoot);
--- reactFoundations/indecision-app/src/app.js
+++ reactFoundations/indecision-app/src/app.js
@@ -1,8 +1,43 @@
 console.log("App.js is running");
 
+var user = {
+  name: "Andrew",
+  age: 26,
+  location: "Texas",
+};
+
+var app = {
+  title: "This is the title",
+  subtitle: "This is a subtitle",
+  options: ["one", "two"],
+};
+var userName = "John Wick";
+var userAge = 43;
+var userLocation = "New York";
+
+function getOptions(app) {
+  return <p>{app.options.length > 0 ? "Options" : "No options"}</p>;
+}
 //JSX-Javascript XML
-var template = <p>This is JSX code with watch!</p>;
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+    <ol>
+      <li>item one</li>
+      <li>item two</li>
+    </ol>
+  </div>
+);
+
+var template2 = (
+  <div>
+    <h1>{user.name.toUpperCase()}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
+  </div>
+);
 
 var appRoot = document.getElementById("app");
-
 ReactDOM.render(template, appRoot);
